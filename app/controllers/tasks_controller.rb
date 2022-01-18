3a52bcc3d13f770c6ec11217fbef99f1c347aa2e
Melia Mazouzi <melia.mazouzi@gmail.com>
2022-01-18 18:47:27 +0100

    feat mark a task as done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,7 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     @list = List.find(params[:list_id])
     @task.list = @list
+    @task.done = false
     if @task.save
       redirect_to list_tasks_path
     end
@@ -21,9 +22,24 @@ class TasksController < ApplicationController
     redirect_to list_tasks_path
   end
 
+  def update
+    @task = Task.find(params[:id])
+    if @task.done?
+      @task.done = false
+    else
+      @task.done = true
+    end
+    @task.update(task_status_params)
+    redirect_to list_tasks_path
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:description)
+    params.require(:task).permit(:description, :done)
+  end
+
+  def task_status_params
+    params.permit(:done)
   end
 end
