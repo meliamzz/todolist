eb4281552b114146dc1a13ba8c3a60d5b2234deb
Melia Mazouzi <melia.mazouzi@gmail.com>
2022-01-18 11:24:49 +0100

    add back-end for new list, and design v0

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,24 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    if @list.save
+      redirect_to list_path(@list)
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
